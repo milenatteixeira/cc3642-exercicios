33e79c6535cc66fb34c015a90d4361f3877a1dff
Milena Teixeira <milenateixeira648@gmail.com>
2020-11-07 18:22:24 -0300

    Projeto iniciado, views criadas.

---
+++ semana-13/bancodedados/src/principal/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+import view.LoginForm;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LoginForm login = new LoginForm();
+        login.setLocationRelativeTo(null);
+        login.setVisible(true);
+    }
+}
